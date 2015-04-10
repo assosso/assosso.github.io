0fa87ca55523e060351672b34faf4a50b0b920a3
Jonathan Perret <jonathan@ut7.fr>
2015-04-10 23:50:02 +0200

    Basic runner.
    sushi, n.:
            When that-which-may-still-be-alive is put on top of rice and
            strapped on with electrical tape.

--- index.js
+++ index.js
@@ -1,101 +1,70 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example',
+ { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.spritesheet('dude', 'dude.png', 32, 48);
-    game.load.image('background', 'background2.png');
+  game.load.spritesheet('dude', 'dude.png', 32, 48);
+  game.load.image('background', 'background2.png');
 
 }
 
 var player;
-var facing = 'left';
+var facing = 'right';
 var jumpTimer = 0;
 var cursors;
 var jumpButton;
-var bg;
 
 function create() {
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.world.setBounds(0, 0, 24000, 600);
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+  game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
 
-    game.physics.arcade.gravity.y = 300;
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    player = game.add.sprite(32, 320, 'dude');
-    game.physics.enable(player, Phaser.Physics.ARCADE);
+  game.physics.arcade.gravity.y = 300;
 
-    player.body.collideWorldBounds = true;
-    player.body.gravity.y = 1000;
-    player.body.maxVelocity.y = 500;
-    player.body.setSize(20, 32, 5, 16);
+  player = game.add.sprite(500, 320, 'dude');
+  game.physics.enable(player, Phaser.Physics.ARCADE);
 
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+  player.body.collideWorldBounds = true;
+  player.body.gravity.y = 1000;
+  player.body.maxVelocity.y = 500;
+  player.body.setSize(20, 32, 5, 16);
 
-    cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  player.animations.add('left', [0, 1, 2, 3], 10, true);
+  player.animations.add('turn', [4], 20, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+  cursors = game.input.keyboard.createCursorKeys();
+  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  player.animations.play('right');
 }
 
 function update() {
 
-    // game.physics.arcade.collide(player, layer);
+  // game.physics.arcade.collide(player, layer);
+  game.camera.x = player.x - 300;
 
-    player.body.velocity.x = 0;
+  player.body.velocity.x = 150;
 
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -150;
+  if (player.body.onFloor()) {
+    player.animations.play('right');
+  } else {
+    player.animations.stop();
+  }
 
-        if (facing != 'left')
-        {
-            player.animations.play('left');
-            facing = 'left';
-        }
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 150;
-
-        if (facing != 'right')
-        {
-            player.animations.play('right');
-            facing = 'right';
-        }
-    }
-    else
-    {
-        if (facing != 'idle')
-        {
-            player.animations.stop();
-
-            if (facing == 'left')
-            {
-                player.frame = 0;
-            }
-            else
-            {
-                player.frame = 5;
-            }
-
-            facing = 'idle';
-        }
-    }
-    
-    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-    {
-        player.body.velocity.y = -500;
-        jumpTimer = game.time.now + 750;
-    }
+  if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+    player.body.velocity.y = -500;
+    jumpTimer = game.time.now + 750;
+  }
 
 }
 
 function render () {
 
-    // game.debug.text(game.time.physicsElapsed, 32, 32);
-    // game.debug.body(player);
-    // game.debug.bodyInfo(player, 16, 24);
+  // game.debug.text(game.time.physicsElapsed, 32, 32);
+  // game.debug.body(player);
+  // game.debug.bodyInfo(player, 16, 24);
 
 }
