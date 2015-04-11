d6c98f5372915eb2a636e2b1dfe9bc32c5b2d08a
Jonathan Perret <jonathan@ut7.fr>
2015-04-11 11:53:04 +0200

    Modularize, add types. Fix characted body box.
    Youth doesn't excuse everything.
                    -- Dr. Janice Lester (in Kirk's body), "Turnabout Intruder",
                       stardate 5928.5.

--- index.ts
+++ index.ts
@@ -1,81 +1,83 @@
 /// <reference path="lib/phaser.comments.d.ts" />
 
-var game = new Phaser.Game(1000, 600, Phaser.CANVAS, 'Assosso',
- { preload: preload, create: create, update: update, render: render });
+module Assosso {
+  var game = new Phaser.Game(1000, 600, Phaser.CANVAS, 'Assosso',
+   { preload: preload, create: create, update: update, render: render });
 
-function preload() {
+  function preload() {
 
-  game.load.spritesheet('bob', 'asset/sprite_perso_run.png', 92, 130);
+    game.load.spritesheet('bob', 'asset/sprite_perso_run.png', 92, 130);
 
-  game.load.image('background', 'background2.png');
+    game.load.image('background', 'background2.png');
 
-}
+  }
 
-var player;
-var facing = 'right';
-var jumpTimer = 0;
-var cursors;
-var jumpButton;
-var rightButton;
-var leftButton;
+  var player: Phaser.Sprite;
+  var facing: string = 'right';
+  var jumpTimer: number = 0;
+  var cursors: Phaser.CursorKeys;
+  var jumpButton: Phaser.Key;
+  var rightButton: Phaser.Key;
+  var leftButton: Phaser.Key;
 
-function create() {
+  function create() {
+    game.world.setBounds(0, 0, 24000, 600);
 
-  game.world.setBounds(0, 0, 24000, 600);
+    game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
 
-  game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 300;
 
-  game.physics.arcade.gravity.y = 300;
+    player = game.add.sprite(500, 320, 'bob');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
 
-  player = game.add.sprite(500, 320, 'bob');
-  game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 1000;
+    player.body.maxVelocity.y = 500;
+    player.body.setSize(92, 130, 0, 0);
 
-  player.body.collideWorldBounds = true;
-  player.body.gravity.y = 1000;
-  player.body.maxVelocity.y = 500;
-  player.body.setSize(92, 130, 5, 16);
+    player.animations.add('right', [0, 1, 2], 10, true);
+    player.animations.add('jump', [3], 10, false);
 
-  player.animations.add('right', [0, 1, 2], 10, true);
-  player.animations.add('jump', [3], 10, false);
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    leftButton = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightButton = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
-  cursors = game.input.keyboard.createCursorKeys();
-  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-  leftButton = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-  rightButton = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-
-  player.animations.play('right');
-}
-
-function update() {
-
-  // game.physics.arcade.collide(player, layer);
-  game.camera.x = player.x - 300;
-
-  player.body.velocity.x = 150;
-
-  if (player.body.onFloor()) {
     player.animations.play('right');
-  } else {
-    player.animations.play('jump');
   }
 
-  if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
-    player.body.velocity.y = -500;
-    jumpTimer = game.time.now + 750;
+  function update() {
+
+    // game.physics.arcade.collide(player, layer);
+    game.camera.x = player.x - 300;
+
+    player.body.velocity.x = 150;
+
+    if (player.body.onFloor()) {
+      player.animations.play('right');
+    } else {
+      player.animations.play('jump');
+    }
+
+    if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+      player.body.velocity.y = -500;
+      jumpTimer = game.time.now + 750;
+    }
+
+    if (rightButton.isDown) {
+      player.body.velocity.x = 300;
+    }
+
   }
 
-  if (rightButton.isDown) {
-    player.body.velocity.x = 300;
+  function render () {
+
+    // game.debug.text(game.time.physicsElapsed, 32, 32);
+    // game.debug.body(player);
+    // game.debug.bodyInfo(player, 16, 24);
+
   }
-
-}
-
-function render () {
-
-  // game.debug.text(game.time.physicsElapsed, 32, 32);
-  // game.debug.body(player);
-  // game.debug.bodyInfo(player, 16, 24);
 
 }
